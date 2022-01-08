7b58ddb47f2ab186202dd64675b79e49f33e127d
Iamnotagenius <kazakk9@gmail.com>
2022-01-08 19:58:27 +0300

    Switched from s-nail to neomutt and downloading script

--- scripts/musictags.sh
+++ scripts/musictags.sh
@@ -1,8 +1,12 @@
 for file in "$@"; do
-	track=(`basename "$file" | cut -f1 -d. | tr ' ' _ | sed 's/_-_/ /' | sed 's/.mp3//' `)
-	id3v2 -a "${track[0]//_/ }" -t "${track[1]//_/ }" "$file"
-	mp3unicode -s utf8 -2 unicode "$file"
-	if [[ $? -ne 0 ]]; then
-		echo Something wrong with $file
-	fi
+    artist=${file%% - *}
+    song=${file#* - }
+    song=${song%.*}
+    echo Artist = $artist\nSong = $song
+    id3tag -a"$artist" -s"$song" "$file"
+    mp3unicode -s utf8 -2 unicode "$file"
+    if [[ $? -ne 0 ]]; then
+        echo Something wrong with "$file"
+    fi
+    ls -d -R ~/EARGASM/* | fzf --filepath-word --prompt="Where to put this song? > " --header-first --header="$artist - $song" | xargs -d \\n mv "$file"
 done
