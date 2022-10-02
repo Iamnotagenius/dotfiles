f54e565fec0e2dc3766a04bf7fa958083098361a
Iamnotagenius <kazakk9@gmail.com>
2022-10-02 17:18:09 +0300

    switch to lua config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,59 @@
+local indent = require "indent"
+local g = vim.g
+local opt = vim.opt
+local cmd = vim.cmd
+local env = vim.env
+local api = vim.api
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.hlsearch = true
+opt.nu = true
+opt.rnu = true
+opt.showcmd = true
+opt.showmode = false
+opt.clipboard = 'unnamedplus'
+opt.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+opt.guifont = 'Fira Code Light:h16,FiraCode Nerd Font:h16'
+opt.background = 'dark'
+g.netrw_liststyle = 3
+require('onedark').load()
+env.NVIM_TUI_ENABLE_TRUE_COLOR = true
+if opt.termguicolors then
+    opt.termguicolors = true
+end
+cmd([[
+augroup ResetCursorShape
+au!
+autocmd VimEnter * :normal :startInsert :stopInsert
+augroup END
+]])
+
+if g.neovide then
+    require('neovide')
+end
+
+g.airline_powerline_fonts = true
+g.airline_theme = 'one'
+g['airline#extensions#tabline#enabled'] = true
+
+g.UltiSnipsSnippetStorageDirectoryForUltiSnipsEdit = '~/.config/nvim/snippets'
+
+
+vim.keymap.set('n', '<Tab>', indent.prepend_indent_current_line)
+vim.keymap.set('n', '<S-Tab>', indent.remove_indent_current_line)
+vim.keymap.set('v', '<Tab>', indent.prepend_indent_visual)
+vim.keymap.set('v', '<S-Tab>', indent.remove_indent_visual)
+
+-- Switch between vim keymaps
+vim.keymap.set({ 'i', 'n', 'v' }, '<m-l>', function ()
+    opt.keymap = opt.keymap:get() == '' and 'russian-jcukenwin' or ''
+    require('lualine').refresh {
+        scope = 'tabpage',
+        place = { 'statusline' }
+    }
+    if api.nvim_get_mode().mode == 'i' then
+        api.nvim_feedkeys('a', 'nt', false)
+    end
+end)
