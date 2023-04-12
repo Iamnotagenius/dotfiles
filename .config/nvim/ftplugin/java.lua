169f23762e71a72bbc89c51f1f97a9e1f69d0b05
Iamnotagenius <kazakk9@gmail.com>
2023-04-12 09:25:10 +0300

    neovim: improve lsp config for java

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -3,16 +3,21 @@ vim.api.nvim_set_hl(0, 'javaExternal', { link = '@macro' })
 local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
 local jdtls = require('jdtls')
 
--- Java lang server
-require('lspconfig').jdtls.setup {
-    cmd = {
-        install_path .. "/bin/jdtls",
-        "--jvm-arg=" .. string.format(
-            "-javaagent:%s",
-            require("mason-registry").get_package("jdtls"):get_install_path() .. "/lombok.jar"
-        ),
-    },
+require('mason-lspconfig').setup_handlers {
+    ["jdtls"] = function()
+        jdtls.start_or_attach {
+            name = "jdtls",
+            cmd = {
+                install_path .. "/bin/jdtls",
+                "--jvm-arg=" .. string.format(
+                    "-javaagent:%s",
+                    require("mason-registry").get_package("jdtls"):get_install_path() .. "/lombok.jar"
+                ),
+            },
+        }
+    end
 }
+
 
 vim.keymap.set('n', '<leader>i', jdtls.organize_imports)
 vim.keymap.set('n', '<leader>v', jdtls.extract_variable)
